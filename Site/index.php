76a0a0c32b7c422cafefb347a27d8efab4a24a0c
Eazzer <l>
2019-07-02 18:13:57 +0200

    Mise au propre, avancé du menu, statistique a ajouter

--- Site/index.php
+++ Site/index.php
@@ -4,21 +4,21 @@
   <meta charset="UTF-8">
   <title>Tickets</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-  <link rel="stylesheet" media="screen" href="./css/SiteAppli.css">
+  <link rel="stylesheet" href="./css/SiteAppli.css">
   <link rel="shortcut icon" href="./img/favicon.png" type="image/x-icon"/>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
 
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+<ul class="menu">
+  <li><a class="active" href="index.php" role="button">Home</a></li>
+  <li><a class="menu" href="newtick.php" role="button">Créer un ticket</a></li>
+  <li><a class="menu" href="tickets.php" role="button">Tous les tickets</a></li>
+  <li><a class="menu" href="admin/index.php" role="button">ADMIN</a></li>
+
+</ul>
 </head>
 <body class="container" align="center">
-
   <h1 style="color:#4444ff;text-align:center;">Squicket</h1>
-  <br><br>
-  <a class="btn btn-primary" href="newtick.php" role="button">Créer un ticket</a>
-  <a class="btn btn-primary" href="tickets.php" role="button">Tous les tickets</a>
-  <a class="btn btn-primary" href="admin/index.php" role="button">ADMIN</a>
-
-
+  <br><br><br><br><br>
+  <p class="titre"> Bienvenue dans squicket ! <br> Tu trouveras toutes les informations utile juste au dessus !</p>
 </body>
 </html>
